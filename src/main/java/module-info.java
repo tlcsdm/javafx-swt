39eb87bfc4e80253f54989a979dff9caef5db6ba
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-10-18 23:55:09 +0800

    build: prepare for 17.0.9

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module javafx.swt {
     requires javafx.base;
     requires javafx.graphics;
-    requires org.eclipse.swt;
+    requires org.eclipse.swt.win32.win32.x86_64;
 
     exports javafx.embed.swt;
 }
